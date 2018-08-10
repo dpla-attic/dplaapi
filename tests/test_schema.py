87f20163d0f1db446315297cbfece1243c87f655
Mark Breedlove <mb@dp.la>
2018-08-10 17:17:08 -0400

    Add test for /schema endpoint

---
+++ tests/test_schema.py
@@ -0,0 +1,12 @@
+from apistar import test
+from dplaapi import app
+
+
+client = test.TestClient(app)
+
+
+def test_schema():
+    response = client.get('/schema/')
+    assert response.status_code == 200
+    data = response.json()
+    assert 'paths' in data
