cb702db4306c7c661ee01d3ee52a10d9f58e04a3
Mark Breedlove <mb@dp.la>
2018-08-14 14:05:04 -0400

    Fix redirect

--- dplaapi/handlers/__init__.py
+++ dplaapi/handlers/__init__.py
@@ -11,7 +11,7 @@ log = logging.getLogger(__name__)
 async def redir_to_recent_version() -> dict:
     """Redirect to the most recent version of the API, /items endpoint"""
     try:
-        headers = {'Location': '/v2:items'}
+        headers = {'Location': '/v2/items'}
         data = headers
         return apistar.http.JSONResponse(data, status_code=301,
                                          headers=headers)
