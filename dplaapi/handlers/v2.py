38446c393e2648a846d964b1734bb373c060594f
Mark Breedlove <mb@dp.la>
2018-08-14 14:03:26 -0400

    Remove validators module and refactor

--- dplaapi/handlers/v2.py
+++ dplaapi/handlers/v2.py
@@ -5,18 +5,27 @@ import requests
 from urllib.parse import quote_plus
 
 import dplaapi
-from dplaapi import validators
 from dplaapi.exceptions import ServerError
 
 
 log = logging.getLogger(__name__)
 
 
+class ItemsType(apistar.types.Type):
+    """Parameter constraints for item searches"""
+    q = apistar.validators.String(
+        title='Search term',
+        description='Search term',
+        min_length=4,
+        max_length=200,
+        allow_null=False)
+
+
 async def items(q: str) -> dict:
     """Get "item" records"""
     try:
-        s = validators.Search(term=q)
-        url = "%s/_search?q=%s" % (dplaapi.ES_BASE, quote_plus(s.term))
+        params = ItemsType(q=q)
+        url = "%s/_search?q=%s" % (dplaapi.ES_BASE, quote_plus(params.q))
         resp = requests.get(url)
         resp.raise_for_status()
         result = resp.json()
