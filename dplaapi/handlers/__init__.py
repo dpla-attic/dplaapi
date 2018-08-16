6785c3725982bf7c2fea9652b3bf63f34bfb356e
Mark Breedlove <mb@dp.la>
2018-08-16 16:11:54 -0400

    Remove unnecessary code, dplaapi.handlers.__init__
    Remove unnecessary try/except. The function does too little for it to be
    necessary.

--- dplaapi/handlers/__init__.py
+++ dplaapi/handlers/__init__.py
@@ -9,11 +9,7 @@ log = logging.getLogger(__name__)
 
 async def redir_to_recent_version() -> dict:
     """Redirect to the most recent version of the API, /items endpoint"""
-    try:
-        headers = {'Location': '/v2/items'}
-        data = headers
-        return apistar.http.JSONResponse(data, status_code=301,
-                                         headers=headers)
-    except Exception:
-        log.exception('Unexpected error')
-        raise ServerError('Unexpected error')
+    headers = {'Location': '/v2/items'}
+    data = headers
+    return apistar.http.JSONResponse(data, status_code=301,
+                                     headers=headers)
