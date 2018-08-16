cd43ecfe198c591f7e24472ee51fff84b52cbc9d
Mark Breedlove <mb@dp.la>
2018-08-16 16:00:18 -0400

    Add test for dplaapi.handlers.__init__

---
+++ tests/handlers/test_handlers.py
@@ -0,0 +1,12 @@
+
+from apistar import test
+from dplaapi import app
+
+
+client = test.TestClient(app)
+
+
+def test_root_redirect():
+    response = client.get('/', allow_redirects=False)
+    assert response.status_code == 301
+    assert response.headers['Location'] == '/v2/items'
