cb20ad5c676c9f8707ec4363236a7b69ba2b254f
Mark Breedlove <mb@dp.la>
2018-08-16 15:23:28 -0400

    Remove experimental functions
    Remove experimental functions that were written for learning API Star

--- dplaapi/handlers/__init__.py
+++ dplaapi/handlers/__init__.py
@@ -18,23 +18,3 @@ async def redir_to_recent_version() -> dict:
     except Exception:
         log.exception('Unexpected error')
         raise ServerError('Unexpected error')
-
-
-async def request_info(request: apistar.http.Request) -> dict:
-    """Get information about the request"""
-    return {
-        'method': request.method,
-        'url': request.url,
-        'headers': dict(request.headers),
-        'body': request.body.decode('utf-8')}
-
-
-async def sysinfo() -> dict:
-    """Get system information"""
-    if os.getenv('DEBUG_SYSINFO'):
-        return {
-            'cpus': os.cpu_count(),
-            'pid': os.getpid(),
-            'ppid': os.getppid()}
-    else:
-        raise apistar.exceptions.Forbidden()
