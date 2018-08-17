5846a0e775685c36b41e778089228eed5200fe46
Mark Breedlove <mb@dp.la>
2018-08-17 15:55:12 -0400

    Fix flake8 errors

--- tests/test_dplaapi_warns_es_base_notset.py
+++ tests/test_dplaapi_warns_es_base_notset.py
@@ -1,6 +1,7 @@
 import os
 import logging
 
+
 def test_dplaapi_warns_if_ES_BASE_not_set(monkeypatch):
 
     def mock_warning(self, message):
@@ -8,4 +9,4 @@ def test_dplaapi_warns_if_ES_BASE_not_set(monkeypatch):
 
     os.putenv('ES_BASE', '')
     monkeypatch.setattr(logging.Logger, 'warning', mock_warning)
-    import dplaapi
+    import dplaapi    # noqa
