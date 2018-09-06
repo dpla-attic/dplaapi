401d41e37bc5367d01b8f745e7f3ba57ef4b4a99
Mark Breedlove <mb@dp.la>
2018-09-06 15:12:48 -0400

    Handle /items path
    For the sake of some applications that expect it, like the frontend,
    serve /v2/items as /items.

--- dplaapi/routes/__init__.py
+++ dplaapi/routes/__init__.py
@@ -1,10 +1,16 @@
 
 from apistar import Route, Include
 from dplaapi import handlers
+from dplaapi.handlers import v2 as v2_handlers
 from . import v2 as v2_routes
 
 
 routes = [
     Route('/', method='GET', handler=handlers.redir_to_recent_version),
-    Include('/v2', name='/v2', routes=v2_routes.routes)
+    Include('/v2', name='/v2', routes=v2_routes.routes),
+    # These paths go to the most recent protocol version of the API; in this
+    # case, /v2:
+    Route('/items', method='GET', handler=v2_handlers.multiple_items),
+    Route('/items/{id_or_ids}', method='GET',
+          handler=v2_handlers.specific_item),
 ]
