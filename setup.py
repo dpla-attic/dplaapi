843430877b3c17d4abd26008f5b7968511c52c56
Mark Breedlove <mb@dp.la>
2018-09-11 15:40:48 -0400

    Fix version specifiers in setup.py
    They were not pinned to patchlevel changes as intended.

--- setup.py
+++ setup.py
@@ -34,16 +34,16 @@ setup(name='dplaapi',
       packages=find_packages(),
       scripts=['bin/devapi'],
       install_requires=[
-          'apistar>=0.5<0.6',
-          'uvicorn~=0.3',
-          'gunicorn~=19.9',
+          'apistar~=0.5.41',
+          'uvicorn~=0.3.2',
+          'gunicorn~=19.9.0',
           # aiofiles is required by ASyncApp, even though we have no static
           # files. It's probably there for the /docs endpoint, which we
           # have disabled.
-          'aiofiles~=0.3',
-          'peewee~=3.6',
-          'psycopg2-binary~=2.7',
-          'boto3~=1.8',
+          'aiofiles~=0.4.0',
+          'peewee~=3.6.0',
+          'psycopg2-binary~=2.7.5',
+          'boto3~=1.8.6',
           'cachetools~=2.1.0'
       ],
       extras_require={
