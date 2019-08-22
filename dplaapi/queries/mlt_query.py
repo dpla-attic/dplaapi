d01942d112311d01d7e00fe2439e0dfe6f634360
AudreyAltman <audreykaltman@gmail.com>
2019-08-21 21:33:15 -0400

    add mlt description

--- dplaapi/queries/mlt_query.py
+++ dplaapi/queries/mlt_query.py
@@ -12,7 +12,7 @@ query_skel = {
     'query': {
         'more_like_this': {
             'fields': [
-                'sourceResource.title.mlt', 'sourceResource.subject.mlt'
+                'sourceResource.title.mlt', 'sourceResource.subject.mlt', 'sourceResource.description.mlt'
             ],
             'min_term_freq': 1,
             'min_doc_freq': 5,
