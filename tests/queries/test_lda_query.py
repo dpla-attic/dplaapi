550140705a30ea72c3f2cf758740895f2bc34008
AudreyAltman <audreykaltman@gmail.com>
2020-02-03 21:14:38 -0500

    fix test

--- tests/queries/test_lda_query.py
+++ tests/queries/test_lda_query.py
@@ -4,11 +4,11 @@ from dplaapi.queries.lda_query import LDAQuery
 from dplaapi.types import LDAQueryType
 
 
-def test_LDAQuery_produces_query_with_like_clause():
+def test_LDAQuery_produces_query_with_queryVector_clause():
     """LDAQuery produces a query with a "vector" clause
     with correct datatype"""
     params = LDAQueryType()
-    params.update({'vector': ['0.1', '0.3'], 'id': 'foo'})
+    params.update({'vector': ['0.1', '0.3'], 'id': 'foo', 'bucket': 'bar'})
     q = LDAQuery(params)
     query_vector = \
         q.query['query']['script_score']['script']['params']['queryVector']
@@ -18,7 +18,7 @@ def test_LDAQuery_produces_query_with_like_clause():
 def test_LDAQuery_has_correct_default_sort():
     """The search query without a sort requested has the correct 'sort'"""
     params = LDAQueryType()
-    params.update({'vector': ['0.1', '0.3'], 'id': 'foo'})
+    params.update({'vector': ['0.1', '0.3'], 'id': 'foo', 'bucket': 'bar'})
     q = LDAQuery(params)
     assert q.query['sort'] == [
         {'_score': {'order': 'desc'}},
@@ -30,6 +30,6 @@ def test_LDAQuery_has_source_clause_for_fields_parameter():
     """If there's a "fields" query param, there's a "_source" property in the
     Elasticsearch query."""
     params = LDAQueryType({'fields': 'id'})
-    params.update({'vector': ['0.1', '0.3'], 'id': 'foo'})
+    params.update({'vector': ['0.1', '0.3'], 'id': 'foo', 'bucket': 'bar'})
     q = LDAQuery(params)
     assert q.query['_source'] == ['id']
